c822924873550b2f0b71b8d4f13db414709f87cd
David Maibach <maibachd@student.ethz.ch>
2020-11-27 12:11:09 +0100

    27.11 work before meeting Devin

--- Simulation_first.py
+++ Simulation_first.py
@@ -1,30 +1,104 @@
 '''First draft of the simulations in the context of a masters thesis
 with Prof. Robert Brandenberger'''
+'''This programm is meant to be used as a foundation for further simulations'''
 
 '''Copyrights to David Maibach'''
 
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-#define constants
+#define constants according to arXiv: 1006.2514v3
+
+#according to The Astrophysical Journal, 622:1356-1362, 2005 April 1, Table 2. Units [cm^3 s^-1]
+def deexitation_crosssection(t_k):
+    if 0<t_k and t_k<1.5:
+        return 1.38e-13
+    if 1.5 < t_k and t_k < 3:
+        return 1.43e-13
+    if 3 < t_k and t_k < 5:
+        return 2.71e-13
+    if 5 < t_k and t_k < 7:
+        return 6.60e-13
+    if 7 < t_k and t_k < 9:
+        return 1.47e-12
+    if 9 < t_k and t_k < 12.5:
+        return 2.88e-12
+    if 12.5 < t_k and t_k < 17.5:
+        return 9.1e-12
+    if 17.5 < t_k and t_k < 22.5:
+        return 1.78e-11
+    if 22.5 < t_k and t_k < 27.5:
+        return 2.73e-11
+    if 27.5 < t_k and t_k < 35:
+        return 3.67e-11
+    if 35 < t_k and t_k < 45:
+        return 5.38e-11
+    if 45 < t_k and t_k < 55:
+        return 6.86e-11
+    if 55 < t_k and t_k < 65:
+        return 8.14e-11
+    if 65 < t_k and t_k < 75:
+        return 9.25e-11
+    if 75 < t_k and t_k < 85:
+        return 1.02e-10
+    if 85 < t_k and t_k < 95:
+        return 1.11e-10
+    if 95 < t_k and t_k < 150:
+        return 1.19e-10
+    if 150 < t_k and t_k < 250:
+        return 1.75e-10
+    if 250 < t_k and t_k < 350:
+        return 2.09e-10
+    else:
+        print('T_K is out of scope for the deexcitation fraction')
+        return 0
+#redshift probing
+z = 100
+#redshift string formation
+z_i = 1000
+#string tension in units of [10^-6]
+gmu_6 = 0.3
+#string speed
+vsgammas_square = 1./3
+#temperature of HI atoms inside the wake [K]
+T_K = 20*gmu_6*vsgammas_square*(z_i+1.)/(z+1)
+#CMB temperature [K]
+T_gamma = 2.725*(1+z)
+#background numberdensity hydrogen [cm^-3]
+nback=1.9e-7 *(1.+z)**3
+#collision coeficcient hydrogen-hydrogen (density in the wake is 4* nback, Delta E for hyperfine is 0.068 [K], A_10 = 2.85e-15 [s^-1])
+xc = 4*nback*deexitation_crosssection(T_K)* 0.068/(2.85e-15 *T_gamma)
+#wake brightness temperature [K]
+T_b = 0.07* xc/(xc+1.)*(1-T_gamma/T_K)*np.sqrt(1.+z)
+#background temperature [K] (assume Omega_b, h, Omega_Lambda, Omega_m as in arXiv: 1405.1452)
+T_back = 0.19055e-3
 
 #define quantities of noise and the patch of the sky
+#patch properties
 patch_size = 256
-power_law = -2.0
-wake_brightness = 1
-patch_angle = 5
+patch_angle = 5.
 angle_per_pixel = patch_angle/patch_size
+#wake properties
+wake_brightness = 0.01
 wake_size_angle = 1
 shift_wake_angle = [0, 0]
+#Gaussian noise properties
+sigma_noise = 1
+mean_noise = 1
+power_law = -2.
 
-#define function for a string signal (assuming it is about 1 deg x 1 deg in size)
-def stringwake(size,intensity, anglewake, angleperpixel,shift):
+#define function for a string signal (assuming it is about wake_size_angle deg x wake_size_angle deg in size)
+def stringwake_PS(size, intensity, anglewake, angleperpixel, shift):
     #coordinate the dimensions of wake and shift
-    patch = np.zeros((size,size))
-    shift_pixel = int(np.round(shift/angleperpixel))
+    patch = np.zeros((size, size))
+    shift_pixel = np.zeros(2)
+    shift_pixel[0] = int(np.round(shift[0]/angleperpixel))
+    shift_pixel[1] = int(np.round(shift[1]/angleperpixel))
     wakesize_pixel = int(np.round(anglewake/angleperpixel))
-    for i in range(size/2+shift_pixel[0]-wakesize_pixel/2,size/2+shift_pixel[0]+wakesize_pixel+1):#TODO: make sure its an integer
-        for j in range(size/2+shift_pixel[1]-wakesize_pixel/2,size/2+shift_pixel[1]+wakesize_pixel+1):#TODO: make sure its an integer
+    print(size/2+shift_pixel[0]-wakesize_pixel/2)
+    print(size/2+shift_pixel[0]+wakesize_pixel/2+1)
+    for i in range(int(size/2+shift_pixel[0]-wakesize_pixel/2), int(size/2+shift_pixel[0]+wakesize_pixel/2+1)):#Todoo: make sure its an integer is not necessary because integer division
+        for j in range(int(size/2+shift_pixel[1]-wakesize_pixel/2), int(size/2+shift_pixel[1]+wakesize_pixel/2+1)):
             patch[i, j] = intensity
     return patch
 
@@ -38,26 +112,29 @@ def fftIndgen(n):
     return a + b
 
 #define function that generates a gaussian random field of size patch_size
-def gaussian_random_field(Pk = lambda k : k**-3.0, size = 100):
+def gaussian_random_field(Pk = lambda k : k**-3.0, size = 100, sigma = 1):
     #create a two dimensional projected power spectrum
     def Pk2(kx, ky):
         if kx == 0 and ky == 0:
             return 0.0
         return np.sqrt(Pk(np.sqrt(kx**2 + ky**2)))
     #create the noise
-    noise = np.fft.fft2(np.random.normal(size = (size, size)))
+    noise = np.fft.fft2(np.random.normal(0, sigma, size = (size, size)))
     #calculate its amplitude
     amplitude = np.zeros((size, size))
     for i, kx in enumerate(fftIndgen(size)):
         for j, ky in enumerate(fftIndgen(size)):
             amplitude[i, j] = Pk2(kx, ky)
-    return np.fft.ifft2(noise * amplitude)
+    #add your signal
+    return np.fft.ifft2(noise * amplitude + np.fft.fft2(stringwake_PS(patch_size, wake_brightness, wake_size_angle,
+                                                                   angle_per_pixel, shift_wake_angle)))
 
 
 #Plot the GRF map for a given size for different power spectra
 for alpha in [-4.0, -3.0, -2.0]:
-    out = gaussian_random_field(Pk = lambda k: k**alpha, size=256)
+    out = gaussian_random_field(Pk = lambda k: k**alpha, size = patch_size, sigma = sigma_noise)
     plt.figure()
     plt.imshow(out.real, interpolation='none')
-    plt.savefig('test_GRF_with'+str(np.abs(alpha))+'.png', dpi=400)
+    plt.show()
+    #plt.savefig('test_GRF_with'+str(np.abs(alpha))+'.png', dpi=400)
 
