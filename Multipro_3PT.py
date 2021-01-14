1be77e4826552984c0590c0bc13cc4634bfffdf1
David Maibach <maibachd@student.ethz.ch>
2021-01-14 17:35:44 +0100

    14.01.12 .... three point improvement, multiprocessing

--- Multipro_3PT.py
+++ Multipro_3PT.py
@@ -3,10 +3,12 @@
 
 # In[8]:
 
-
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import multiprocessing
+
 patch_size = 512
 patch_angle = 5. #in degree
 angle_per_pixel = patch_angle/patch_size
@@ -43,20 +45,17 @@ def sort_ft(field):
             dummy[N-(k-int(N/2))-1][l-int(N/2)] = field[k][l]
     return dummy
 
-n = 10
-bins = 300
-threepoint_average = np.array(np.zeros(n), dtype=complex)
-threepoint_average_signal = np.array(np.zeros(n), dtype=complex)
-for j in range(0, len(threepoint_average)):
+
+def multiprocessing_fun(j, threepoint_average):
+    np.random.seed(j)
     grf = np.random.normal(0, 1, (N, N))
-    kx, ky = np.meshgrid(2 * math.pi *np.fft.fftfreq(N, c), 2 * math.pi * np.fft.fftfreq(N, c))
-    mag_k = np.sqrt(kx**2 + ky**2)
-    pspec_noise = power_spectrum(mag_k, 2, 1)
+    kx, ky = np.meshgrid(2 * math.pi * np.fft.fftfreq(N, c), 2 * math.pi * np.fft.fftfreq(N, c))
+    mag_k = np.sqrt(kx ** 2 + ky ** 2)
     for i in range(0, N):
         ky[0][i] = 0.001
     for i in range(0, N):
         kx[i][0] = 0.001
-    ft_sig = signal_ft(kx , ky)
+    ft_sig = signal_ft(kx, ky)
     ft_signal = (np.fft.fft2(grf) * power_spectrum(mag_k, 2, 1) ** .5 + ft_sig)
     ft = (np.fft.fft2(grf) * power_spectrum(mag_k, 2, 1) ** .5)
     ft_ordered = sort_ft(ft)
@@ -65,18 +64,42 @@ for j in range(0, len(threepoint_average)):
     threepoint_signal = 0
     for k in range(0, N):
         for l in range(0, N):
-            threepoint += ft_ordered[k][l]*ft_ordered[N-k-1][N-l-1]*ft_ordered[N-l-1][k]
-            threepoint_signal += ft_ordered_signal[k][l] * ft_ordered_signal[N - k - 1][N - l - 1] * ft_ordered_signal[N - l - 1][k]
-    threepoint_average[j] = threepoint/N**2
+            threepoint += ft_ordered[k][l] * ft_ordered[N - k - 1][N - l - 1] * ft_ordered[N - l - 1][k]
+            threepoint_signal += ft_ordered_signal[k][l] * ft_ordered_signal[N - k - 1][N - l - 1] * \
+                                 ft_ordered_signal[N - l - 1][k]
+    threepoint_average[j] = threepoint / N ** 2
     print(threepoint/N**2)
-    threepoint_average_signal[j] = threepoint_signal/ N ** 2
+    print(threepoint_average[j])
+    threepoint_average_signal[j] = threepoint_signal / N ** 2
+
+def combine_complex(a,b):
+    global thhree_point_average
+    for i in range(0,len(threepoint_average)):
+
+
+
+n = 10
+bins = 300
+
+threepoint_average_r = multiprocessing.Array('d',range(n))
+threepoint_average_r = multiprocessing.Array('d',range(n))
+threepoint_average = np.array(np.zeros(n), dtype=complex)
+threepoint_average_signal = np.array(np.zeros(n), dtype=complex)
+processes = []
+for i in range(0, n):
+    p = multiprocessing.Process(target=multiprocessing_fun, args=(i, threepoint_average_r))
+    processes.append(p)
+    p.start()
+for process in processes:
+    process.join()
+
 print(np.abs(np.mean(threepoint_average)))
 print(np.abs(np.mean(threepoint_average_signal)))
-plt.hist(threepoint_average.real, bins=80)
-plt.savefig('test_3PF.png', dpi=400)
-plt.clf()
-plt.hist(threepoint_average_signal.real, bins=80)
-plt.savefig('test_3PF_with_sign.png', dpi=400)
+#plt.hist(threepoint_average.real, bins=80)
+#plt.savefig('test_3PF.png', dpi=400)
+#plt.clf()
+#plt.hist(threepoint_average_signal.real, bins=80)
+#plt.savefig('test_3PF_with_sign.png', dpi=400)
 
 
 
